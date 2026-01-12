6a3bb7aa96ba1809092bffb33f1eec3ce9c8d645
Andrés López Zamarreño <92918157+andreslopezzamarreno@users.noreply.github.com>
2026-01-12 14:38:37 +0100

    Delete recetasZamlor directory

--- recetasZamlor/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { ExplorerComponent } from './explorer/explorer';
-import { FileViewerComponent } from './file-viewer/file-viewer';
-
-export const routes: Routes = [
-  { path: '', component: ExplorerComponent },
-  { path: 'file/:owner/:repo/:path', component: FileViewerComponent },
-];
